c05862b1f1d0a0910c8f7174c1bef617a54c1367
steveSuave <s.levantes@gmail.com>
2020-12-27 00:02:56 +0200

    reduce with a functional tinge

---
+++ structure/accumulate.scm
@@ -0,0 +1,25 @@
+;; a.k.a. reduce etc.
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(fold-right / 1 '(1 2 3))
+;; (/ 1 (/ 2 (/ 3 1)))
+;; Value: 3/2
+
+(fold-left / 1 '(1 2 3))
+;; (/ (/ 1 2) 3)
+;; Value: 1/6
+
